ee4a14bb25ba416e2418982e520ec4da03a30415
aliandpeach <aliandpeach@gmail.com>
2021-05-15 09:10:13 +0800

    commit

--- collector-server/src/test/java/com/yk/telnet/test/ConnectionPoolTest.java
+++ collector-server/src/test/java/com/yk/telnet/test/ConnectionPoolTest.java
@@ -7,9 +7,21 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class ConnectionPoolTest
 {
@@ -47,5 +59,314 @@ public class ConnectionPoolTest
             }
         }).start();
         Thread.currentThread().join();
+    }
+
+    /**
+     * 分批依次执行线程 1
+     */
+    @Test
+    public void testRunThreadInBatches() throws InterruptedException
+    {
+        int total = 100;
+        int runCount = 5;
+        AtomicInteger counter = new AtomicInteger(0);
+        Queue<RunThread> list = new LinkedBlockingQueue<>();
+        for (int i = 0; i < total; i++)
+        {
+            RunThread th = new RunThread();
+            th.setCounter(counter);
+            list.offer(th);
+        }
+
+        while (list.size() != 0)
+        {
+            CountDownLatch down = new CountDownLatch(runCount);
+            for (int i = 0; i < runCount; i++)
+            {
+                RunThread th = list.poll();
+                if (null == th)
+                {
+                    // (total / runCount) 不一定是整除
+                    continue;
+                }
+                th.setDown(down);
+                th.start();
+            }
+            down.await();
+        }
+        Thread.currentThread().join();
+    }
+
+    /**
+     * 分批依次执行线程 2
+     * <p>
+     * 利用 CyclicBarrier 的特性
+     */
+    @Test
+    public void testCyclicBarrier() throws InterruptedException
+    {
+        int total = 100;
+        int runCount = 5;
+
+        AtomicInteger counter = new AtomicInteger(0);
+        Queue<RunThreadBarrier> list = new LinkedBlockingQueue<>();
+        for (int i = 0; i < total; i++)
+        {
+            RunThreadBarrier th = new RunThreadBarrier();
+            th.setCounter(counter);
+            list.offer(th);
+        }
+
+        ExecutorService service = Executors.newFixedThreadPool(runCount * 2);
+
+        List<String> result = new ArrayList<>();
+
+        while (list.size() != 0)
+        {
+            List<Future<String>> futures = new ArrayList<>();
+            CountDownLatch down = new CountDownLatch(1);
+            CyclicBarrier cyclicBarrier = new CyclicBarrier(runCount, new Run(counter, down));
+            for (int i = 0; i < runCount; i++)
+            {
+                RunThreadBarrier th = list.poll();
+                if (null == th)
+                {
+                    // (total / runCount) 不一定是整除
+                    continue;
+                }
+//                th.setBarrier(cyclicBarrier);
+                futures.add(service.submit(th));
+            }
+            futures.forEach(f ->
+            {
+                try
+                {
+                    System.out.println(f.get());
+                }
+                catch (InterruptedException | ExecutionException e)
+                {
+                    e.printStackTrace();
+                }
+            });
+        }
+        Thread.currentThread().join();
+    }
+
+    private class Run implements Runnable
+    {
+        private AtomicInteger counter;
+        private CountDownLatch down;
+
+        public Run(AtomicInteger counter, CountDownLatch down)
+        {
+            this.counter = counter;
+            this.down = down;
+        }
+
+        @Override
+        public void run()
+        {
+            System.out.println("完成" + counter.get());
+            down.countDown();
+        }
+    }
+
+    /**
+     * 分批依次执行线程 2.1
+     * <p>
+     * 利用 CyclicBarrier 的特性
+     */
+    @Test
+    public void testCyclicBarrier2_1() throws InterruptedException
+    {
+        int total = 20;
+        int runCount = 5;
+
+        AtomicInteger counter = new AtomicInteger(0);
+        Queue<RunThreadBarrier> list = new LinkedBlockingQueue<>();
+        for (int i = 0; i < total; i++)
+        {
+            RunThreadBarrier th = new RunThreadBarrier();
+            th.setCounter(counter);
+            list.offer(th);
+        }
+
+        ExecutorService service = Executors.newFixedThreadPool(runCount * 2);
+
+        List<String> result = new ArrayList<>();
+
+        int group = total % runCount == 0 ? total / runCount : total / runCount + 1;
+
+        IntStream.range(0, group).forEach(t ->
+        {
+            List<CompletableFuture<String>> futures = new ArrayList<>();
+            for (int i = 0; i < runCount; i++)
+            {
+                RunThreadBarrier th = list.poll();
+                if (null == th)
+                {
+                    // (total / runCount) 不一定是整除
+                    continue;
+                }
+                futures.add(CompletableFuture.supplyAsync(th::call, service));
+            }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+            futures.forEach(f ->
+            {
+                try
+                {
+                    result.add(f.get());
+                }
+                catch (InterruptedException | ExecutionException e)
+                {
+                    e.printStackTrace();
+                }
+            });
+
+            System.out.println("完成" + counter.get());
+        });
+        System.out.println(result);
+    }
+
+    /**
+     * 分批依次执行线程 3
+     */
+    @Test
+    public void testRunThreadInBatches3() throws InterruptedException
+    {
+        int total = 100;
+        int runCount = 5;
+        long start = System.currentTimeMillis();
+        AtomicInteger counter = new AtomicInteger(0);
+        BlockingQueue<String> list = new LinkedBlockingQueue<>(5);
+
+        ExecutorService service = Executors.newFixedThreadPool(runCount * 2);
+
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        IntStream.range(0, total).forEach(t ->
+        {
+            futures.add(CompletableFuture.runAsync(() ->
+            {
+                try
+                {
+                    synchronized (ConnectionPoolTest.class)
+                    {
+                        while (list.size() == runCount)
+                        {
+                            ConnectionPoolTest.class.wait();
+                        }
+                    }
+                    list.put("1");
+
+                    TimeUnit.SECONDS.sleep(2);
+                    counter.incrementAndGet();
+                    System.out.println(counter.get() + " end");
+
+                    list.take();
+                    synchronized (ConnectionPoolTest.class)
+                    {
+                        ConnectionPoolTest.class.notifyAll();
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }, service));
+        });
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        long end = System.currentTimeMillis();
+        System.out.println("总数" + total + " 个请求, 分批异步执行, 完成时间  = {}" + (end - start));
+    }
+
+    private static class RunThread extends Thread
+    {
+        private CountDownLatch down;
+
+        private AtomicInteger counter;
+
+        public void setCounter(AtomicInteger counter)
+        {
+            this.counter = counter;
+        }
+
+        public void setDown(CountDownLatch down)
+        {
+            this.down = down;
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                System.out.println(Thread.currentThread().getName() + " start");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println(Thread.currentThread().getName() + " end");
+                counter.incrementAndGet();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                down.countDown();
+            }
+        }
+    }
+
+
+    private static class RunThreadBarrier implements Callable<String>
+    {
+        private AtomicInteger counter;
+
+        private CyclicBarrier barrier;
+
+        public void setBarrier(CyclicBarrier barrier)
+        {
+            this.barrier = barrier;
+        }
+
+        public void setCounter(AtomicInteger counter)
+        {
+            this.counter = counter;
+        }
+
+        @Override
+        public String call()
+        {
+            try
+            {
+//                System.out.println(Thread.currentThread().getName() + " start");
+                TimeUnit.SECONDS.sleep(2);
+//                System.out.println(Thread.currentThread().getName() + " end");
+                return "success" + counter.incrementAndGet();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+                return "failed" + counter.get();
+            }
+            finally
+            {
+                if (null != barrier)
+                {
+                    try
+                    {
+                        barrier.await();
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    catch (BrokenBarrierException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
---
+++ collector-server/src/test/java/com/yk/telnet/test/QueueTest.java
@@ -0,0 +1,50 @@
+package com.yk.telnet.test;
+
+import org.junit.Test;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.IntStream;
+
+public class QueueTest
+{
+    @Test
+    public void testOffer()
+    {
+        BlockingQueue<String> link = new LinkedBlockingQueue<>(2);
+        link.offer("1");
+        link.offer("2");
+        link.offer("3"); //不会等待,直接返回false
+        System.out.println(link);
+    }
+
+    @Test
+    public void testPut() throws InterruptedException
+    {
+        BlockingQueue<String> link = new LinkedBlockingQueue<>(2);
+        link.put("1");
+        link.put("2");
+        link.put("3"); //一直等待
+        System.out.println(link);
+    }
+
+    @Test
+    public void testAdd()
+    {
+        BlockingQueue<String> link = new LinkedBlockingQueue<>(2);
+        link.add("1");
+        link.add("2");
+        link.add("3"); //抛出异常
+        System.out.println(link);
+    }
+
+    @Test
+    public void testTake() throws InterruptedException
+    {
+        IntStream.range(0, 10).forEach(t -> System.out.println(t));
+        System.out.println(1 % 5);
+        BlockingQueue<String> link = new LinkedBlockingQueue<>(2);
+        link.take(); //一直等待
+        System.out.println(link);
+    }
+}
