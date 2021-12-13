17078be6d1497bf0c338ff3e076783f339057443
aliandpeach <aliandpeach@gmail.com>
2021-12-13 09:02:28 +0800

    commit

--- collector-server/src/test/java/com/yk/telnet/test/AwaitTest.java
+++ collector-server/src/test/java/com/yk/telnet/test/AwaitTest.java
@@ -8,19 +8,17 @@ import java.util.concurrent.TimeUnit;
 
 public class AwaitTest
 {
-    // service.shutdown();
-    // shutdown之后不能再提交 会抛出异常
-    // service.submit(() -> System.out.println(2));
-
-    // 单独只使用awaitTermination(15, TimeUnit.SECONDS); 那么整个线程池会等待设置的时间
+    // 单独只使用awaitTermination(15, TimeUnit.SECONDS);  主线程会等待15s, 之后继续往下执行,此时线程池还存活 继续提交线程; 因此awaitTermination配合shutdown使用时, 不能放在前面,否则要先等待15s
     @Test
     public void main() throws InterruptedException
     {
         ExecutorService service = Executors.newFixedThreadPool(3);
-        service.submit(() -> {System.out.println(1);
+        service.submit(() ->
+        {
             try
             {
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("over");
             }
             catch (InterruptedException e)
             {
@@ -28,9 +26,23 @@ public class AwaitTest
             }
         });
 
-        // awaitTermination之后 线程池还存活 继续可以使用
-        // service.shutdown(); // 执行 shutdown请求后, 调用awaitTermination可确保线程池内部所有线程执行完毕后关闭，除非内部线程执行时间超过timeout
-        boolean is = service.awaitTermination(15, TimeUnit.SECONDS);
+        service.shutdown(); // 阻止新来的任务提交，对已经提交了的任务不会产生任何影响
+        try
+        {
+            if (!service.awaitTermination(15, TimeUnit.SECONDS)) // awaitTermination 可确保线程池内部所有线程执行完毕后关闭，除非内部线程执行时间超过timeout
+            {
+                service.shutdownNow(); //阻止新来的任务提交，同时会中断当前正在运行的线程
+                if (!service.awaitTermination(15, TimeUnit.SECONDS))
+                {
+                    System.out.println("Pool did not terminate in " + 15 + " seconds");
+                }
+            }
+        }
+        catch (InterruptedException ie)
+        {
+            service.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
 
         boolean isShutdown = service.isShutdown();
         System.out.println(isShutdown);
